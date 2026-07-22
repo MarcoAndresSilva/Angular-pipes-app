48ca8bba67af63cb178e6a02b1fc735c79f9791b
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-21 23:35:07 -0400

    rotes pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Basic pages pipes basicos',
+    loadComponent: () => import('./pages/basic-page/basic-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Custom pages pipes personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page'),
+  },
+  {
+    path: 'number',
+    title: 'Number pages pipes numericos',
+    loadComponent: () => import('./pages/number-page/number-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Uncommon pages pipes poco comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
