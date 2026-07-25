9de9201a80dfd0f74f6fb905f99788148b33e4b4
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-24 22:02:13 -0400

    feat: add pipes de strings tolower upper titlecase advance

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,8 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe],
   templateUrl: './basic-page.html',
 })
-export default class BasicPage {}
+export default class BasicPage {
+  nameLower = signal('marco silva');
+  nameUpper = signal('MARCO SILVA');
+  fullName = signal('MaRcO sILva');
+}
