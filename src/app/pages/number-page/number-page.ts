6046e17de282743b297c92abc4796ec57822cacb
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-27 23:49:15 -0400

    number pipes

--- src/app/pages/number-page/number-page.ts
+++ src/app/pages/number-page/number-page.ts
@@ -1,8 +1,14 @@
+import { DecimalPipe, PercentPipe, CurrencyPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './number-page.html',
 })
-export default class NumberPage {}
+export default class NumberPage {
+  totalSells() {
+    return 2567789.5567;
+  }
+  percent = 0.4856;
+}
