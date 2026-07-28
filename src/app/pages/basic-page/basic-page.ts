7639dc4eb57cf4240f42bc51d40ab7f89b03d161
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-28 00:17:10 -0400

    Date pipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,13 +1,22 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
 })
 export default class BasicPage {
   nameLower = signal('marco silva');
   nameUpper = signal('MARCO SILVA');
   fullName = signal('MaRcO sILva');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect(() => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
 }
