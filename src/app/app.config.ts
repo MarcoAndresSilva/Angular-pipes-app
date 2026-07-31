6b23bd2b3556241b925f9fcf50fa1aa59589cbab
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-30 23:59:58 -0400

    Locale pipes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,9 +9,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
-
+import localeFr from '@angular/common/locales/fr';
 registerLocaleData(localeEs, 'es');
-
+registerLocaleData(localeFr, 'fr');
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -19,7 +19,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     {
       provide: LOCALE_ID,
-      useValue: 'es',
+      useValue: 'fr',
     },
   ],
 };
